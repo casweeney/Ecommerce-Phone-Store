115a60fb84766a296be7c2280210806cf7733784
Casweeney <casweeno2000@gmail.com>
2019-11-14 22:26:34 +0100

    product details and add to cart components completed

--- src/context.js
+++ src/context.js
@@ -7,16 +7,52 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
     state = {
-        products:storeProducts,
-        detailProduct:detailProduct
-    }
-    handleDetail = () => {
-        console.log("hello from detail");
+        products:[],
+        detailProduct:detailProduct,
+        cart:[]
+    };
+
+    componentWillMount(){
+        this.setProducts();
     }
 
-    addToCart = () => {
-        console.log("hello from add to cart");
-    }
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = {...item};
+            tempProducts = [...tempProducts,singleItem]
+        });
+
+        this.setState(() => {
+            return {products:tempProducts}
+        });
+    };
+
+    getItem = (id) => {
+        const product = this.state.products.find(item => item.id === id);
+        return product;
+    };
+
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(() => {
+            return {detailProduct:product}
+        });
+    };
+
+    addToCart = (id) => {
+        let tempProducts = [...this.state.products];
+        const index = tempProducts.indexOf(this.getItem(id));
+        const product = tempProducts[index];
+        product.inCart = true;
+        product.count = 1;
+        const price = product.price;
+        product.total = price;
+
+        this.setState(() => {
+            return { products: tempProducts,cart:[...this.state.cart,product] }
+        },()=>{console.log(this.state)})
+    };
 
     render() {
         return (
