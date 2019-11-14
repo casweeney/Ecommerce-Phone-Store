f1827a78063c948be590e2a7c2b3deafa1e01888
Casweeney <casweeno2000@gmail.com>
2019-11-14 15:35:55 +0100

    product page and component completed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
+import {ProductConsumer} from '../context';
+
 class ProductList extends Component {
-    state = {
-        products:[]
-    }
     render() {
         return (
             <React.Fragment>
                     <div className="py-5">
                         <div className="container">
                             <Title name="our" title="product" />
-                            <div className="row">
 
+                            <div className="row">
+                                <ProductConsumer>
+                                    {(value)=>{
+                                        return value.products.map( product => {
+                                            return <Product key={product.id} product={product} />;
+                                        })
+                                    }}
+                                </ProductConsumer>
                             </div>
                         </div>
                     </div>
